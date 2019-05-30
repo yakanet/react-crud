4a1d3dc16fe9623bf083fdf3db3d954d1e6cee19
Mathieu Broutin <mbroutin@sedona.fr>
2019-05-30 08:12:00 +0200

    feat: add navigation

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,12 +1,20 @@
 import React from 'react';
 import './App.css';
 import {Header} from './header/Header';
+import {BrowserRouter, Route, Switch} from 'react-router-dom';
+import {Todo} from './todo/Todo';
+import {Comment} from './comment/Comment'
 
 const App: React.FC = () => {
-  return (
-    <Header/>
-
-  );
-}
+    return (
+        <BrowserRouter>
+            <Header/>
+            <Switch>
+                <Route path='/todo' component={Todo}/>
+                <Route path='/comment' component={Comment}/>
+            </Switch>
+        </BrowserRouter>
+    );
+};
 
 export default App;
--- src/app/header/Header.tsx
+++ src/app/header/Header.tsx
@@ -1,14 +1,17 @@
 import * as React from 'react';
+import {Link} from 'react-router-dom';
 
 export class Header extends React.Component {
     render(): React.ReactNode {
         return (
             <div>
                 <header className='header'>
-                    <span>LOGO</span>
+                    <span>
+                        <Link to='/'>LOGO</Link>
+                    </span>
                     <ul>
-                        <li>CRUD TODO</li>
-                        <li>CRUD COMMENTS</li>
+                        <li><Link to='/todo'>CRUD TODO</Link></li>
+                        <li><Link to='/comment'>CRUD COMMENTS</Link></li>
                     </ul>
                 </header>
 
